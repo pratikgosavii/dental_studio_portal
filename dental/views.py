684b5cf143665f56baf17fc716925da4c294dc68
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2022-04-16 14:00:33 +0530

    update

--- dental/views.py
+++ dental/views.py
@@ -7,28 +7,6 @@ from main.models import *
 # @login_required(login_url='login')
 def index(request):
 
-    if request.user.is_superuser:
+   
 
-        pay = [] 
-
-        orders_count = order_detials.objects.all().count()
-
-        payment_data = payments.objects.all()
-
-        doctors_data = doctor.objects.all().count()
-
-        for i in payment_data:
-            pay.append(i.amount)
-
-        amount = sum(pay)
-        
-
-        context = {
-            
-            'orders_count' : orders_count,
-            'amount' : amount,
-            'doctors_data' : doctors_data,
-            
-        }
-
-        return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html')
